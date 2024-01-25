e152bc077e36f5d5cba67922052332f92b93c6ae
Martino Facchin <m.facchin@arduino.cc>
2024-01-25 15:06:24 +0100

    Initial modules: button and buzzer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,4 +1,3 @@
-/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
   * @file           : main.c
@@ -15,54 +14,32 @@
   *
   ******************************************************************************
   */
-/* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
+#include <stdbool.h>
 
 /* Private variables ---------------------------------------------------------*/
 
 I2C_HandleTypeDef hi2c1;
-
 IWDG_HandleTypeDef hiwdg;
 
-/* USER CODE BEGIN PV */
+#define NODE_BUTTONS    0x7C
+#define NODE_BUZZER     0x3C
 
-/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_I2C1_Init(void);
+static void MX_I2C1_Init(uint8_t address);
 static void MX_IWDG_Init(void);
 static void MX_NVIC_Init(void);
-/* USER CODE BEGIN PFP */
+static uint8_t readPinstraps();
 
-/* USER CODE END PFP */
+static volatile bool dataReceived = false;
+static uint8_t i2c_buffer[128];
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
+static uint8_t ADDRESS;
 
 /**
   * @brief  The application entry point.
@@ -70,46 +47,114 @@ static void MX_NVIC_Init(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
   /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_I2C1_Init();
-  MX_IWDG_Init();
-
+  ADDRESS = readPinstraps();
+  MX_I2C1_Init(ADDRESS);
+  //MX_IWDG_Init();
   /* Initialize interrupts */
   MX_NVIC_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
+  configurePins();
+
+  HAL_I2C_EnableListen_IT(&hi2c1);
 
   /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+    //HAL_IWDG_Refresh(&hiwdg);
+    if (dataReceived) {
+      switch (ADDRESS) {
+        case NODE_BUTTONS:
+          HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, i2c_buffer[0] == 0 ? GPIO_PIN_RESET: GPIO_PIN_SET);
+          HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, i2c_buffer[1] == 0 ? GPIO_PIN_RESET: GPIO_PIN_SET);
+          HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, i2c_buffer[2] == 0 ? GPIO_PIN_RESET: GPIO_PIN_SET);
+          break;
+        case NODE_BUZZER:
+          HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, i2c_buffer[0] == 0 ? GPIO_PIN_RESET: GPIO_PIN_SET);
+          break;
+      }
+      dataReceived = false;
+    }
   }
-  /* USER CODE END 3 */
+}
+
+void configurePins() {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  switch (ADDRESS) {
+    case NODE_BUTTONS:
+      GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
+      GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+      GPIO_InitStruct.Pull = GPIO_NOPULL;
+      GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+      HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+      break;
+    case NODE_BUZZER:
+      GPIO_InitStruct.Pin = GPIO_PIN_0;
+      GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+      GPIO_InitStruct.Pull = GPIO_NOPULL;
+      GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+      HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+      break;
+    }
+}
+
+uint8_t populateBuffer() {
+  switch (ADDRESS) {
+    case NODE_BUTTONS:
+      i2c_buffer[0] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0);
+      i2c_buffer[1] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1);
+      i2c_buffer[2] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_2);
+      return 3;
+      break;
+  }
+}
+
+uint8_t prepareRx() {
+  switch (ADDRESS) {
+    case NODE_BUTTONS:
+      return 3;
+    case NODE_BUZZER:
+      return 1;
+  }
+}
+
+void HAL_I2C_SlaveTxCpltCallback(I2C_HandleTypeDef *hi2c) {
+
+}
+
+void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c) {
+  dataReceived = true;
+}
+
+void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, uint16_t AddrMatchCode) {
+
+  if (TransferDirection == I2C_DIRECTION_RECEIVE) {
+    uint8_t len = populateBuffer();
+    HAL_I2C_Slave_Seq_Transmit_IT(&hi2c1, i2c_buffer, len, I2C_FIRST_AND_LAST_FRAME);
+  } else {
+    uint8_t len = prepareRx();
+    HAL_I2C_Slave_Seq_Receive_IT(&hi2c1, i2c_buffer, len, I2C_FIRST_AND_LAST_FRAME);
+  }
+}
+
+void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c) {
+    HAL_I2C_EnableListen_IT(&hi2c1);
+}
+
+void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c) {
+  /** Error_Handler() function is called when error occurs.
+    * 1- When Slave doesn't acknowledge its address, Master restarts communication.
+    * 2- When Master doesn't acknowledge the last data transferred, Slave doesn't care
+    */
+  if (HAL_I2C_GetError(hi2c) != HAL_I2C_ERROR_AF)
+  {
+    Error_Handler();
+  }
 }
 
 /**
@@ -166,23 +211,13 @@ static void MX_NVIC_Init(void)
   * @param None
   * @retval None
   */
-static void MX_I2C1_Init(void)
+static void MX_I2C1_Init(uint8_t address)
 {
-
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
   hi2c1.Init.Timing = 0x00100413;
-  hi2c1.Init.OwnAddress1 = 68;
+  hi2c1.Init.OwnAddress1 = address;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_ENABLE;
-  hi2c1.Init.OwnAddress2 = 240;
-  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
   hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_ENABLE;
   hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
   if (HAL_I2C_Init(&hi2c1) != HAL_OK)
@@ -203,10 +238,6 @@ static void MX_I2C1_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
 }
 
 /**
@@ -216,14 +247,6 @@ static void MX_I2C1_Init(void)
   */
 static void MX_IWDG_Init(void)
 {
-
-  /* USER CODE BEGIN IWDG_Init 0 */
-
-  /* USER CODE END IWDG_Init 0 */
-
-  /* USER CODE BEGIN IWDG_Init 1 */
-
-  /* USER CODE END IWDG_Init 1 */
   hiwdg.Instance = IWDG;
   hiwdg.Init.Prescaler = IWDG_PRESCALER_4;
   hiwdg.Init.Window = 4095;
@@ -232,10 +255,6 @@ static void MX_IWDG_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN IWDG_Init 2 */
-
-  /* USER CODE END IWDG_Init 2 */
-
 }
 
 /**
@@ -245,31 +264,37 @@ static void MX_IWDG_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
-
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+}
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+static uint8_t readPinstraps() {
 
-  /*Configure GPIO pin : PA0 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  // address = PA6 | PA7 | PA8 | PC14 | PC15 | PF2
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  GPIO_InitStruct.Pin = GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = GPIO_PIN_14 | GPIO_PIN_15;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = GPIO_PIN_2;
+  HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  return (
+    HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_6) << 6 |
+    HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_7) << 5 |
+    HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8) << 4 |
+    HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_14) << 3 |
+    HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15) << 2 |
+    HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_2) << 1);
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
 
 /**
   * @brief  This function is executed in case of error occurrence.
