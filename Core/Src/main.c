3a650271ee9b11e49caab01c721aa0b311422ec6
Martino Facchin <m.facchin@arduino.cc>
2024-02-01 10:38:05 +0100

    node_buttons: allow controlling the LEDs as output

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -140,7 +140,7 @@ void configurePins() {
   switch (ADDRESS) {
     case NODE_BUTTONS:
       GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
-      GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+      GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
       GPIO_InitStruct.Pull = GPIO_NOPULL;
       GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
       HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
