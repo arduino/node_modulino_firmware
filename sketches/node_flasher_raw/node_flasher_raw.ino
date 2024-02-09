e2fbb874c66133b16f131ca7c7b2c1f71bc724c3
Martino Facchin <m.facchin@arduino.cc>
2024-02-09 18:17:03 +0100

    improve test sketch

--- sketches/node_flasher_raw/node_flasher_raw.ino
+++ sketches/node_flasher_raw/node_flasher_raw.ino
@@ -1,3 +1,6 @@
+#include "ArduinoGraphics.h"
+#include "Arduino_LED_Matrix.h"
+
 #include "Wire.h"
 #include "fw.h"
 
@@ -5,6 +8,8 @@
 
 int howmany;
 int res;
+
+ArduinoLEDMatrix matrix;
 
 
 int command_write_page(uint8_t opcode, uint8_t* buf_cmd, size_t len_cmd, const uint8_t* buf_fw, size_t len_fw) {
@@ -16,7 +21,7 @@ int command_write_page(uint8_t opcode, uint8_t* buf_cmd, size_t len_cmd, const u
   if (len_cmd > 0) {
     buf_cmd[len_cmd - 1] = 0;
     for (int i = 0; i < len_cmd - 1; i++) {
-      buf_cmd[len_cmd - 1] ^=  buf_cmd[i];
+      buf_cmd[len_cmd - 1] ^= buf_cmd[i];
     }
     Wire1.endTransmission(true);
     Wire1.requestFrom(100, 1);
@@ -127,10 +132,19 @@ final_ack:
 }
 
 int sendReset() {
-  uint8_t buf[3] = {'D', 'I', 'E'};
-  Wire1.beginTransmission(62);
-  Wire1.write(buf, 3);
-  return Wire1.endTransmission();
+  uint8_t buf[3] = { 'D', 'I', 'E' };
+  int ret;
+  for (int i = 30; i < 128; i++) {
+    Wire1.beginTransmission(i);
+    ret = Wire1.endTransmission();
+    if (ret != 2) {
+      Wire1.beginTransmission(i);
+      Wire1.write(buf, 40);
+      ret = Wire1.endTransmission();
+      return ret;
+    }
+  }
+  return ret;
 }
 
 void setup() {
@@ -146,6 +160,25 @@ void setup() {
   int resp;
   Serial.println("GET_COMMAND");
   resp = command(0, nullptr, 0, resp_buf, 20);
+
+  if (resp < 0) {
+    Serial.println("Failed :(");
+    matrix.begin();
+    matrix.beginDraw();
+
+    matrix.stroke(0xFFFFFFFF);
+    matrix.textScrollSpeed(100);
+
+    const char text[] = "FAIL";
+    matrix.textFont(Font_4x6);
+    matrix.beginText(0, 1, 0xFFFFFF);
+    matrix.println(text);
+    matrix.endText(SCROLL_LEFT);
+
+    matrix.endDraw();
+    while (1)
+      ;
+  }
 
   for (int i = 0; i < resp; i++) {
     Serial.println(resp_buf[i], HEX);
@@ -164,7 +197,7 @@ void setup() {
   }
 
   Serial.println("MASS_ERASE");
-  uint8_t erase_buf[3] = {0xFF, 0xFF, 0x0};
+  uint8_t erase_buf[3] = { 0xFF, 0xFF, 0x0 };
   resp = command(0x44, erase_buf, 3, nullptr, 0);
   for (int i = 0; i < resp; i++) {
     Serial.println(resp_buf[i], HEX);
@@ -173,13 +206,27 @@ void setup() {
   for (int i = 0; i < node_base_bin_len; i += 128) {
     Serial.print("WRITE_PAGE ");
     Serial.println(i, HEX);
-    uint8_t write_buf[5] = {8, 0, i / 256, i % 256};
+    uint8_t write_buf[5] = { 8, 0, i / 256, i % 256 };
     resp = command_write_page(0x32, write_buf, 5, &node_base_bin[i], 128);
     for (int i = 0; i < resp; i++) {
       Serial.println(resp_buf[i], HEX);
     }
     delay(10);
   }
+
+  matrix.begin();
+  matrix.beginDraw();
+
+  matrix.stroke(0xFFFFFFFF);
+  matrix.textScrollSpeed(100);
+
+  const char text[] = "PASS";
+  matrix.textFont(Font_4x6);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.println(text);
+  matrix.endText(SCROLL_LEFT);
+
+  matrix.endDraw();
 
   //Wire1.beginTransmission(100);
   //if (Wire1.endTransmission() == 0) {
@@ -224,10 +271,8 @@ void setup() {
     Serial.println(Wire1.read(), HEX);
     }
   */
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
