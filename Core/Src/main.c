15aa4d23cf7764fcd3a25bc364121d5b8ba36400
Martino Facchin <m.facchin@arduino.cc>
2024-04-04 16:56:22 +0200

    buttons: set input pins as INPUT_PULLDOWN

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -221,10 +221,15 @@ void configurePins() {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
   switch (PINSTRAP_ADDRESS) {
     case NODE_BUTTONS:
-      GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5;
+      GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
+      GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+      GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+      GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+      HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+      GPIO_InitStruct.Pin = GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5;
       GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
       GPIO_InitStruct.Pull = GPIO_NOPULL;
-      GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
       HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
       break;
     case NODE_BUZZER:
