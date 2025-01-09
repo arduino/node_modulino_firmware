08f796ea1db3cdd4aff05d4772285ad014a4dcac
Martino Facchin <m.facchin@arduino.cc>
2025-01-09 17:26:29 +0100

    support joystick with adc
    ADC conversion is "suboptimal" due to space limitations.
    The best approach would be using DMA

--- Core/Src/stm32c0xx_hal_msp.c
+++ Core/Src/stm32c0xx_hal_msp.c
@@ -124,38 +124,6 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
 }
 
 /**
-* @brief I2C MSP De-Initialization
-* This function freeze the hardware resources used in this example
-* @param hi2c: I2C handle pointer
-* @retval None
-*/
-void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
-{
-  if(hi2c->Instance==I2C1)
-  {
-  /* USER CODE BEGIN I2C1_MspDeInit 0 */
-
-  /* USER CODE END I2C1_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_I2C1_CLK_DISABLE();
-
-    /**I2C1 GPIO Configuration
-    PB6     ------> I2C1_SCL
-    PB7     ------> I2C1_SDA
-    */
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_6);
-
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_7);
-
-    /* I2C1 interrupt DeInit */
-    HAL_NVIC_DisableIRQ(I2C1_IRQn);
-  /* USER CODE BEGIN I2C1_MspDeInit 1 */
-
-  /* USER CODE END I2C1_MspDeInit 1 */
-  }
-}
-
-/**
 * @brief TIM_Encoder MSP Initialization
 * This function configures the hardware resources used in this example
 * @param htim_encoder: TIM_Encoder handle pointer
@@ -246,28 +214,68 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
   }
 
 }
-/**
-* @brief TIM_PWM MSP De-Initialization
-* This function freeze the hardware resources used in this example
-* @param htim_pwm: TIM_PWM handle pointer
-* @retval None
-*/
-void HAL_TIM_PWM_MspDeInit(TIM_HandleTypeDef* htim_pwm)
+
+
+void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
 {
-  if(htim_pwm->Instance==TIM1)
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+  if(hadc->Instance==ADC1)
   {
-  /* USER CODE BEGIN TIM1_MspDeInit 0 */
+  /* USER CODE BEGIN ADC1_MspInit 0 */
 
-  /* USER CODE END TIM1_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_TIM1_CLK_DISABLE();
-  /* USER CODE BEGIN TIM1_MspDeInit 1 */
+  /* USER CODE END ADC1_MspInit 0 */
 
-  /* USER CODE END TIM1_MspDeInit 1 */
+  /** Initializes the peripherals clocks
+  */
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+    PeriphClkInit.AdcClockSelection = RCC_ADCCLKSOURCE_SYSCLK;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+      return;
+    }
+
+    /* Peripheral clock enable */
+    __HAL_RCC_ADC_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**ADC1 GPIO Configuration
+    PA0     ------> ADC1_IN0
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN ADC1_MspInit 1 */
+
+  /* USER CODE END ADC1_MspInit 1 */
+
   }
 }
-
-
 /* USER CODE BEGIN 1 */
+
+void configureADC(ADC_HandleTypeDef* hadc) {
+  hadc->Instance = ADC1;
+  hadc->Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV1;
+  hadc->Init.Resolution = ADC_RESOLUTION_8B;
+  hadc->Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc->Init.ScanConvMode = ADC_SCAN_SEQ_FIXED;
+  hadc->Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc->Init.LowPowerAutoWait = DISABLE;
+  hadc->Init.LowPowerAutoPowerOff = DISABLE;
+  hadc->Init.ContinuousConvMode = ENABLE;
+  hadc->Init.NbrOfConversion = 2;
+  hadc->Init.DiscontinuousConvMode = DISABLE;
+  hadc->Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc->Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc->Init.DMAContinuousRequests = DISABLE;
+  hadc->Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  hadc->Init.SamplingTimeCommon1 = ADC_SAMPLETIME_160CYCLES_5;
+  hadc->Init.OversamplingMode = DISABLE;
+  hadc->Init.TriggerFrequencyMode = ADC_TRIGGER_FREQ_HIGH;
+
+  HAL_ADC_Init(hadc);
+}
 
 /* USER CODE END 1 */
--- Core/Src/stm32c0xx_it.c
+++ Core/Src/stm32c0xx_it.c
@@ -56,6 +56,7 @@
 
 /* External variables --------------------------------------------------------*/
 extern I2C_HandleTypeDef hi2c1;
+extern ADC_HandleTypeDef hadc1;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -156,6 +157,14 @@ void I2C1_IRQHandler(void)
   /* USER CODE BEGIN I2C1_IRQn 1 */
 
   /* USER CODE END I2C1_IRQn 1 */
+}
+
+void ADC1_IRQHandler(void)
+{
+  /* USER CODE BEGIN ADC1_IRQn 0 */
+
+  /* USER CODE END ADC1_IRQn 0 */
+  HAL_ADC_IRQHandler(&hadc1);
 }
 
 /* USER CODE BEGIN 1 */
