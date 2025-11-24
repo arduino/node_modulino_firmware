716c77f125d125700b51fd148780e46f485ec966
Sebastian Romero <s.romero@arduino.cc>
2025-11-24 15:34:15 +0100

    Allow specifying debug mode from build script

--- build.sh
+++ build.sh
@@ -7,14 +7,22 @@ echo "Please run git submodule init && git submodule update"
 exit 1
 fi
 
+# Check if debug mode is enabled (-d)
+if [ "$1" == "-d" ]; then
+    echo "Debug mode enabled"
+    DEBUG=1
+else
+    DEBUG=0
+fi
+
 make cleanall # Clean previous builds including binary files
 
-CPU=-mcpu=cortex-m0 make
+CPU=-mcpu=cortex-m0 DEBUG_MODE=$DEBUG make
 echo const > sketches/libraries/Modulino/src/fw.h
 xxd -i build/bin/node_base.bin >> sketches/libraries/Modulino/src/fw.h
 
 make clean
-CPU=-mcpu=cortex-m0 TARGET=matrix_node_base EXTRA_CFLAGS=-DFORCE_LEDMATRIX_MODULINO make
+CPU=-mcpu=cortex-m0 DEBUG_MODE=$DEBUG TARGET=matrix_node_base EXTRA_CFLAGS=-DFORCE_LEDMATRIX_MODULINO make
 echo const > sketches/libraries/Modulino/src/fw_ledmatrix.h
 xxd -i build/bin/matrix_node_base.bin >> sketches/libraries/Modulino/src/fw_ledmatrix.h
 
