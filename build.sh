945a82db7eccb2710c77119d915d04052585cc43
Sebastian Romero <s.romero@arduino.cc>
2025-11-24 15:33:59 +0100

    Separate led matrix and base build

--- build.sh
+++ build.sh
@@ -2,24 +2,20 @@
 
 # Make sure sketches/libraries/Modulino/src/ folder exists
 
-if [ -d sketches/libraries/Modulino/src/ ]; then
-CPU=-mcpu=cortex-m0 make clean
-CPU=-mcpu=cortex-m0 make
-echo const > sketches/libraries/Modulino/src/fw.h
-cd build
-xxd -i node_base.bin >> ../sketches/libraries/Modulino/src/fw.h
-cd ..
-
-CPU=-mcpu=cortex-m0 make clean
-CPU=-mcpu=cortex-m0 EXTRA_CFLAGS=-DFORCE_LEDMATRIX_MODULINO make
-echo const > sketches/libraries/Modulino/src/fw_ledmatrix.h
-cd build
-mv node_base.bin matrix_node_base.bin
-xxd -i matrix_node_base.bin >> ../sketches/libraries/Modulino/src/fw_ledmatrix.h
-cd ..
-
-echo "Now you can create a commit in Modulino library"
-else
+if [ ! -d sketches/libraries/Modulino/src/ ]; then
 echo "Please run git submodule init && git submodule update"
 exit 1
 fi
+
+make cleanall # Clean previous builds including binary files
+
+CPU=-mcpu=cortex-m0 make
+echo const > sketches/libraries/Modulino/src/fw.h
+xxd -i build/bin/node_base.bin >> sketches/libraries/Modulino/src/fw.h
+
+make clean
+CPU=-mcpu=cortex-m0 TARGET=matrix_node_base EXTRA_CFLAGS=-DFORCE_LEDMATRIX_MODULINO make
+echo const > sketches/libraries/Modulino/src/fw_ledmatrix.h
+xxd -i build/bin/matrix_node_base.bin >> sketches/libraries/Modulino/src/fw_ledmatrix.h
+
+echo "Now you can create a commit in Modulino library"
