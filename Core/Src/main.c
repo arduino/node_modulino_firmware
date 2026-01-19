b116f24fef2a5e4884306a3b6b8b2f857c355039
Sebastian Romero <s.romero@arduino.cc>
2026-01-19 17:59:34 +0100

    Initialize all pins used by matrix

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -376,7 +376,7 @@ void configurePins() {
       HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
       break;
     case NODE_LEDMATRIX:
-      GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+      GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_11 | GPIO_PIN_12;
       GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
       GPIO_InitStruct.Pull = GPIO_NOPULL;
       GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
