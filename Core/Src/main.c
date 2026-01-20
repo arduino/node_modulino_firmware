7c9635461f1d734387a1fef892aa5a99f36c65ea
Sebastian Romero <s.romero@arduino.cc>
2026-01-20 12:23:20 +0100

    Adjust tim1 prescaler for 24 MHz used by vibro and buzzer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -631,7 +631,7 @@ static void MX_TIM1_PWM_Init(void)
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
 
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 0;
+  htim1.Init.Prescaler = 1; // Adjusted for 24MHz: Div by 2 to keep 12MHz timebase for PWM formulas
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 65535;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
