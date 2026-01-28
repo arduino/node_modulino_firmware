4fd7efbc2d66bf9f36f9b1a60ff08fb57b47082a
Sebastian Romero <s.romero@arduino.cc>
2026-01-28 14:43:09 +0100

    Exclude path name from array name

--- build.sh
+++ build.sh
@@ -19,11 +19,11 @@ make cleanall # Clean previous builds including binary files
 
 CPU=-mcpu=cortex-m0 DEBUG_MODE=$DEBUG make
 echo const > sketches/libraries/Modulino/src/fw.h
-xxd -i build/bin/node_base.bin >> sketches/libraries/Modulino/src/fw.h
+xxd -i -n node_base.bin build/bin/node_base.bin >> sketches/libraries/Modulino/src/fw.h
 
 make clean
 CPU=-mcpu=cortex-m0 DEBUG_MODE=$DEBUG TARGET=matrix_node_base EXTRA_CFLAGS=-DFORCE_LEDMATRIX_MODULINO make
 echo const > sketches/libraries/Modulino/src/fw_ledmatrix.h
-xxd -i build/bin/matrix_node_base.bin >> sketches/libraries/Modulino/src/fw_ledmatrix.h
+xxd -i -n matrix_node_base.bin build/bin/matrix_node_base.bin >> sketches/libraries/Modulino/src/fw_ledmatrix.h
 
 echo "Now you can create a commit in Modulino library"
